93ff7e0a770cc5c902a6ed799711d9b2365762af
Brandan Richardson <brandanr@gmail.com>
2018-04-12 15:28:31 -0700

    added typed.js to sub-heading of slider

--- js/script.js
+++ js/script.js
@@ -4,4 +4,13 @@ $(document).ready(function () {
         play: 5000,
         pagination: false
     }); 
+
+    var typed = new Typed(".typed", {
+        strings: ["Full-Stack Web Developer.",
+         "Software Engineer.", "Life-long Learner."],
+         typeSpeed: 70,
+         loop: true,
+         startDelay: 1000,
+         showCursor: false
+    });
 });
