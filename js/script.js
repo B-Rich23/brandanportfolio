99d0a0b62c34a97c47cdabbb93cac2b742a0803e
Brandan Richardson <brandanr@gmail.com>
2018-04-15 00:05:25 -0700

    added navigation scrolling to sticky navbar

--- js/script.js
+++ js/script.js
@@ -94,4 +94,12 @@ $(document).ready(function () {
         });
         return false;
     });
+
+    $("#navigation li a").click(function(e){
+        e.preventDefault();
+
+        var targetElement = $(this).attr("href");
+        var targetPosition = $(targetElement).offset().top;
+        $("html, body").animate({scrollTop: targetPosition - 50 }, "slow");
+    });
 });
